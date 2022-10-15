948346c1969dc6e7c6006efd40c92237700f9990
itherdev <iisistiharoh59@gmail.com>
2022-10-15 20:15:54 +0700

    validasi

--- public/sb-admin-2/js/demo/chart-pie-demo.js
+++ public/sb-admin-2/js/demo/chart-pie-demo.js
@@ -2,14 +2,14 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-// Pie Chart Example
+// Pie Chart Status Ketenagaan Guru 
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["PNS", "Non PNS"],
     datasets: [{
-      data: [55, 30, 15],
+      data: [10, 30],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
@@ -33,3 +33,71 @@ var myPieChart = new Chart(ctx, {
     cutoutPercentage: 80,
   },
 });
+
+
+// Pie Chart Status Sertifikasi
+var ctx = document.getElementById("statusSertifikasiGuru");
+var statusSertifikasiGuru = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Sudah", "Belum"],
+    datasets: [{
+      data: [60, 40],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+
+
+// Pie Chart Status Jumlah siswa
+// Pie Chart Status Sertifikasi
+var ctx = document.getElementById("jumlahsiswa");
+var jumlahsiswa = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Laki-laki", "Perempuan"],
+    datasets: [{
+      data: [60, 40],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+

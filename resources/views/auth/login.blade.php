d2d3daf18bcdac98c51f50e9b5b0480d92e4179b
itherdev <iisistiharoh59@gmail.com>
2022-10-17 01:07:51 +0700

    modified login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,53 @@
 <x-guest-layout>
-    
     <div class="container">
-
-        <!-- Outer Row -->
         <div class="row justify-content-center">
-
-            <div class="col-lg-7">
-
+            <div class="col-xl-10 col-lg-12 col-md-9">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
                         <div class="row">
-                            {{-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> --}}
-                            <div class="col">
+                            <!-- Card image -->
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <!-- Card text login -->
+                            <div class="col-lg-6">
                                 <div class="p-5">
+
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang</h1>
+                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                                            style="width: 185px;" alt="logo">
+                                        <h4 class="mt-1 mb-5 pb-1">Logo Sekolah</h4>
+                                    </div>
+                                   
+                                    <div class="text-center">
+                                        <p>LOGIN</p>
                                     </div>
                                     @error('email')
                                     <div class="alert alert-warning alert-dismissible fade show" role="alert">
                                         {{ $message }}
                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                          <span aria-hidden="true">&times;</span>
+                                            <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
                                     @enderror
                                     <form class="user" method="post" action="{{ route('login') }}">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" name="email" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" name="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
-                                        </div>
-                                        <button class="btn btn-primary btn-user btn-block" type="submit">Login</button>
+                                            @csrf
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user"
+                                                    id="exampleInputEmail" name="email" aria-describedby="emailHelp"
+                                                    placeholder="Enter Email Address...">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" name="password"
+                                                    class="form-control form-control-user" id="exampleInputPassword"
+                                                    placeholder="Password">
+                                            </div>
+                                            <button class="btn btn-primary btn-user btn-block" type="submit">Login</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
-
     </div>
 </x-guest-layout>

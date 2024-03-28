77635f30361017689903a1e494974bd9e046058a
Muhammad Mu'min Azis <muhammadmuminaziz@gmail.com>
2024-03-29 06:06:49 +0700

    change url path in pdf view

--- resources/views/print/teacher/index.blade.php
+++ resources/views/print/teacher/index.blade.php
@@ -41,14 +41,14 @@
         <table width="100%" class="header">
             <tr style="">
                 <td rowspan="2" width="100px" style="">
-                    <img src="{{ storage_path('app/public/img/logo-pemprov.png') }}" alt="pemprov maluku" height="50">
+                    <img src="{{ asset('img/logo-pemprov.png') }}" alt="pemprov maluku" height="50">
                 </td>
                 <td style="background-color: gray; padding: 7px; ">
                     <h3 style="margin: 0; text-align: center; color: white;">BALAI TEKNOLOGI DAN KOMUNIKASI PENDIDIKAN
                         PROVINSI MALUKU</h3>
                 </td>
                 <td rowspan="2" width="100px" style="">
-                    <img src="{{ storage_path('app/public/img/tutwuri-handayani.png') }}" alt="tutwuri handayani" height="50">
+                    <img src="{{ asset('img/tutwuri-handayani.png') }}" alt="tutwuri handayani" height="50">
                 </td>
             </tr>
             <tr style="">
